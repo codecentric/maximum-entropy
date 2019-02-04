25f0714430a861c4bef00b41b7cab80efe7c30dc
dominikballreich <dominik.ballreich@codecentric.de>
2019-02-04 11:09:48 +0100

    Added new prediction

--- code/maximum_entropy_helpers.py
+++ code/maximum_entropy_helpers.py
@@ -21,83 +21,41 @@ def get_indicator_normalization_constant_constarint(l, points, weights, prior, r
         indicator = np.append(indicator, np.zeros(len(points) - int(nearest_u)))
         constraint = constraint - (l[i] * indicator)
     normalization = np.sum((prior * np.exp(constraint)) * weights)
+
     return indicator, normalization, constraint
+
 
 def riemann_int(number_of_points, boundary):
     weight = (boundary[1]-boundary[0])/(number_of_points-1)
     points = np.arange(boundary[0], boundary[1], weight)
     points = np.append(points, boundary[1])
     weights = np.ones(number_of_points)*weight
+
     return points, weights
+
 
 def max_ent_estimate(l,*args):
     points = args[0]
     weights = args[1]
     prior = args[2]
     rules = args[3]
-
-    #constraint = 0
-    #for i in range(0, len(rules)):
-    #    rule = rules[i]
-    #    bound_d = rule[0]
-    #    bound_u = rule[1]
-
-    #    if bound_d == '-inf':
-    #        bound_d = np.min(points) - 1
-    #    if bound_u == 'inf':
-    #        bound_u = np.max(points) + 1
-
-        #nearest_l = np.argmin(np.abs(points - bound_d))
-        #nearest_u = np.argmin(np.abs(points - bound_u))
-        #indicator = np.append(np.zeros(int(nearest_l)), np.ones(int(nearest_u) - int(nearest_l)))
-        #indicator = np.append(indicator, np.zeros(len(points) - int(nearest_u)))
-        #constraint = constraint - (l[i] * indicator)
-    #normalization = np.sum((prior * np.exp(constraint)) * weights)
-
     indicator, normalization, constraint = get_indicator_normalization_constant_constarint(l, points, weights, prior, rules)
 
     results = []
     for i in range(0, len(rules)):
         rule = rules[i]
-        #bound_d = rule[0]
-        #bound_u = rule[1]
-
-        #if bound_d == '-inf':
-        #    bound_d = np.min(points) - 1
-        #if bound_u == 'inf':
-        #    bound_u = np.max(points) + 1
-
-        #nearest_l = np.argmin(np.abs(points - bound_d))
-        #nearest_u = np.argmin(np.abs(points - bound_u))
-        #indicator = np.append(np.zeros(int(nearest_l)), np.ones(int(nearest_u) - int(nearest_l)))
-        #indicator = np.append(indicator, np.zeros(len(points) - int(nearest_u)))
         result_lagrange = np.sum((indicator * (prior * np.exp(-l[i] * indicator)) * 1/normalization * weights)) - rule[2]
         results.append(result_lagrange)
 
     return np.sum(np.abs(results))
 
+
 def get_max_ent_dist(l,points, weights, prior, rules):
     indicator, normalization, constraint = get_indicator_normalization_constant_constarint(l, points, weights, prior, rules)
-    #constraint = 0
-    #for i in range(0, len(rules)):
-    #    rule = rules[i]
-    #    bound_d = rule[0]
-    #    bound_u = rule[1]
-
-    #    if bound_d == '-inf':
-    #        bound_d = np.min(points)-1
-    #    if bound_u == 'inf':
-    #        bound_u = np.max(points)+1
-
-    #    nearest_l = np.argmin(np.abs(points - bound_d))
-    #    nearest_u = np.argmin(np.abs(points - bound_u))
-    #    indicator = np.append(np.zeros(int(nearest_l)), np.ones(int(nearest_u) - int(nearest_l)))
-    #    indicator = np.append(indicator, np.zeros(len(points) - int(nearest_u)))
-    #    constraint = constraint - (l[i] * indicator)
-    #normalization = np.sum((prior * np.exp(constraint)) * weights)
     max_ent_dist = (prior * np.exp(constraint)) * 1 / normalization
 
     return max_ent_dist
+
 
 def opt_max_ent(rules,prior_samples):
     kde = sm.nonparametric.KDEUnivariate(np.transpose(np.matrix(prior_samples)))
@@ -110,8 +68,9 @@ def opt_max_ent(rules,prior_samples):
 
     arguments = (points, weights, prior, rules)
     l, fopt = pso(max_ent_estimate, lb, ub, args=arguments, debug=True, phip=0.5, phig=0.5, omega=0.5,
-                           minfunc=1e-12, minstep=0.001, maxiter=7000, swarmsize=500)
+                           minfunc=1e-12, minstep=0.001, maxiter=100, swarmsize=500)
     max_ent_dist = get_max_ent_dist(l, points, weights, prior, rules)
+
     return points, weights, prior, max_ent_dist
 
 
--- code/optimize_prophet.py
+++ code/optimize_prophet.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 import maximum_entropy_helpers as me
 
+# Load data
 y = pd.read_csv('/Users/dominikballreich/PycharmProjects/maximum_entropy/data/AirPassengers.csv')
 y.columns = ['ds', 'y']
+
 
 # Plot the timeseries and mark the test period
 labels = np.arange(49,61)
@@ -18,7 +20,8 @@ plt.ylabel('Number of Passengers')
 plt.axvline(x=131, c='k', linestyle='dashed', linewidth=0.5)
 plt.show()
 
-# Generate forecast for 1960 by using Facebook Prophet.
+
+# Generate forecasts for 1960 by using Facebook Prophet.
 y_train = y.iloc[0:132, :]
 y_test = y.iloc[132:, :]
 m = Prophet(mcmc_samples=1000, seasonality_mode='multiplicative')
@@ -26,30 +29,69 @@ m.fit(y_train)
 future = m.make_future_dataframe(periods=y_test.shape[0], freq='MS')
 forecast = m.predict(future)
 
-#Extract the samples from the posterior predictive distribution for July 1960
+
+# Extract the samples from the posterior predictive distribution for July 1960
 pred_samples = m.predictive_samples(future)
 pred_samples_df = pd.DataFrame(pred_samples['yhat'])
 prior_samples = pred_samples_df.iloc[138, :]
 
-# Plot the kernel density of the posterior predictive distribution for July 1960
-rules = [['-inf', 548, 0.01], [590,'inf', 0.75]]
+
+# Define the constraints for July 1960
+rules = [['-inf', 548, 0.01], [590,'inf', 0.80]]
+
+
+# Calculate the Maximum Entropy Distribution for July 1960
 points, weights, prior, max_ent_dist = me.opt_max_ent(rules, prior_samples)
+
+
+# Plot prior and the Maximum Entropy Distribution for July 1960
 plt.plot(points, prior, label='$\widehat{p_{0}}(y)$')
-plt.plot(points, max_ent_dist)
+plt.plot(points, max_ent_dist, label='Maximum Entropy Distribution')
 plt.xlabel('Number of Passengers')
 plt.ylabel('Density')
 plt.legend()
 plt.show()
 
+
+# Calculate the Maximum Entropy forecast for July 1960
 prediction = np.sum(points * max_ent_dist * weights)
 forecast_new = forecast['yhat'].copy()
 forecast_new.iloc[138] = prediction
 
+
+# Extract the samples from the posterior predictive distribution for May 1960
+pred_samples = m.predictive_samples(future)
+pred_samples_df = pd.DataFrame(pred_samples['yhat'])
+prior_samples = pred_samples_df.iloc[136, :]
+
+
+# Define the constraints for May 1960
+rules = [['-inf', 420, 0.01], [460, 490, 0.7]]
+
+
+# Calculate the Maximum Entropy Distribution for May 1960
+points, weights, prior, max_ent_dist = me.opt_max_ent(rules, prior_samples)
+
+
+# Plot prior and the Maximum Entropy Distribution for May 1960
+plt.plot(points, prior, label='$\widehat{p_{0}}(y)$')
+plt.plot(points, max_ent_dist, label='Maximum Entropy Distribution')
+plt.xlabel('Number of Passengers')
+plt.ylabel('Density')
+plt.legend()
+plt.show()
+
+
+# Calculate the Maximum Entropy forecast for May 1960
+prediction = np.sum(points * max_ent_dist * weights)
+forecast_new.iloc[136] = prediction
+
+
 labels = np.arange(49,61)
 xt = np.arange(0,143,12)
 plt.plot(y['y'], c='b', label='Original')
-plt.plot(forecast['yhat'], c='c', label='Forecasts')
-plt.plot(forecast_new, c='r')
+plt.plot(forecast['yhat'], c='c', label='FB Prophet Forecasts')
+plt.plot(forecast_new, c='m', label='Maximum Entropy Forecasts')
 locs, labs = plt.xticks()
 plt.xticks(xt, labels)
 plt.xlabel('Year')
