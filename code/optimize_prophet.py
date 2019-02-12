86b58645d6eba0c4a1ef5cb09994772bd54c6bae
dominikballreich <dominik.ballreich@codecentric.de>
2019-02-12 14:20:38 +0100

    telling the user to close the plot

--- code/optimize_prophet.py
+++ code/optimize_prophet.py
@@ -18,7 +18,9 @@ plt.xticks(xt, labels)
 plt.xlabel('Year')
 plt.ylabel('Number of Passengers')
 plt.axvline(x=131, c='k', linestyle='dashed', linewidth=0.5)
+print("Close plot")
 plt.show()
+
 
 
 # Generate forecasts for 1960 by using Facebook Prophet.
@@ -30,8 +32,8 @@ future = m.make_future_dataframe(periods=y_test.shape[0], freq='MS')
 forecast = m.predict(future)
 
 # Calculate the RMSE for the Facebook Prophet forecast for May and July 1960
-RMSE_May_July = np.sqrt(np.mean(np.square(forecast['yhat'][136]-y['y'][136])+np.square(forecast['yhat'][138]-y['y'][138])))
-print(RMSE_May_July)
+RMSE_May_July_FBP = np.sqrt(np.mean(np.square(forecast['yhat'][136]-y['y'][136])+np.square(forecast['yhat'][138]-y['y'][138])))
+print("The Facebbook Prophet RMSE for May/July 1960 is:", RMSE_May_July_FBP)
 
 # Extract the samples from the posterior predictive distribution for May 1960
 pred_samples = m.predictive_samples(future)
@@ -54,7 +56,9 @@ plt.xlabel('Number of Passengers')
 plt.ylabel('Density')
 plt.legend()
 plt.title('Prior and Maximum Entropy Distribution for May 1960')
+print("Close plot")
 plt.show()
+
 
 
 # Calculate the Maximum Entropy forecast for May 1960
@@ -70,7 +74,7 @@ prior_samples = pred_samples_df.iloc[138, :]
 
 
 # Define the constraints for July 1960
-rules = [['-inf', 548, 0.01], [590,'inf', 0.80]]
+rules = [[598, 'inf', 0.80]]
 
 
 # Calculate the Maximum Entropy Distribution for July 1960
@@ -84,6 +88,7 @@ plt.xlabel('Number of Passengers')
 plt.ylabel('Density')
 plt.legend()
 plt.title('Prior and Maximum Entropy Distribution for July 1960')
+print("Close plot")
 plt.show()
 
 
@@ -93,7 +98,7 @@ forecast_new.iloc[138] = prediction
 
 # Calculate the RMSE for the Maximum Entropy forecast for May and July 1960
 RMSE_May_July_Max_Ent = np.sqrt(np.mean(np.square(forecast_new[136]-y['y'][136])+np.square(forecast_new[138]-y['y'][138])))
-print(RMSE_May_July_Max_Ent)
+print("The Maximium Entropy RMSE for May/July 1960 is:", RMSE_May_July_Max_Ent)
 
 # Plot the forecasts
 y_p = y['y'][120:]
@@ -104,14 +109,18 @@ labels = [59, 60]
 xt = np.arange(120, 133, 12)
 plt.plot(y_p, c='b', label='Original')
 plt.plot(forcast_p, c='c', label='FB Prophet Forecasts')
-plt.plot(forecast_new_p, c='m', label='Maximum Entropy Forecasts')
+#plt.plot(forecast_new_p, c='m', label='Maximum Entropy Forecasts')
 locs, labs = plt.xticks()
 plt.xticks(xt, labels)
 plt.xlabel('Year')
 plt.ylabel('Number of Passengers')
 plt.axvline(x=131, c='k', linestyle='dashed', linewidth=0.5)
-plt.plot(136, forecast['yhat'][136], 'ro', fillstyle='none')
-plt.plot(138, forecast['yhat'][138], 'ro', fillstyle='none')
+plt.plot(136, forecast['yhat'][136], 'ro', fillstyle='none', color='c')
+plt.plot(138, forecast['yhat'][138], 'ro', fillstyle='none', color='c')
+plt.plot(136, forecast_new.iloc[136], 'x', fillstyle='none', color='r', label='Maximum Entropy Forecasts')
+plt.plot(138, forecast_new.iloc[138], 'x', fillstyle='none', color='r')
+plt.annotate(s='', xy=(136, forecast_new.iloc[136]), xytext=(136, forecast['yhat'][136]), arrowprops=dict(arrowstyle='simple',color='g'))
+plt.annotate(s='', xy=(138, forecast_new.iloc[138]), xytext=(138, forecast['yhat'][138]), arrowprops=dict(arrowstyle='simple',color='g'))
 plt.legend()
 plt.show()
 
